bb9b3b959c6193eeb6a877d7f7713ac22dd54631
David Lo <umiqlo.moe@gmail.com>
2018-02-14 17:13:52 +0800

    update views

--- mp_app/views.py
+++ mp_app/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.core import serializers
 from .models import *
 # Create your views here.
+import mp_app.util.CommonUtil as util
 
-# test http request
-def testHttp(request):
-    return HttpResponse('test Request')
-# test json request
-def testJson(request):
-    return JsonResponse({'debug':'json mode2'})
+def index(request):
+    return HttpResponse('Hello World, This is Home Page. ' + str(util.current_timestamp()))
+    
+def getUserInfoByUserId(request, userId):
+    try:
+        result = User.objects.get(id=userId).toJSON()
+        return util.json_encode(result)
+    except:
+        return util.json_encode_errorMsg("User Not Found")
+
+def getAllMusic(request):
+    try:
+        results = Music.objects.all().values('id', 'name', 'user')
+        return util.json_encode(list(results))
+    except:
+        return util.json_encode_errorMsg("Music Not Found")
